bb4f7545c15ddff0182320ea340192d322d4f7dc
renjiesui <renjiesui@yangqianguan.com>
2022-08-26 18:21:31 +0800

    "update."

--- timerange.py
+++ timerange.py
@@ -6,16 +6,17 @@ import datetime
 argv = sys.argv 
 ignore = True 
 
-if argv.__len__ == 1:
-   print("need date") 
-   exit(2)
+cmpDate = datetime.datetime.now() - datetime.timedelta(minutes=15)
+
+if argv.__len__ > 1:
+    cmpDate = datetime.datetime.strptime(argv[1], "%Y-%m-%d %H:%M:%S")
+
 
 for line in sys.stdin:
     if line.startswith("2022"):
         it = line[0:19]
         date = datetime.datetime.strptime(it, "%Y-%m-%d %H:%M:%S")
-        t = datetime.datetime.strptime(argv[1], "%Y-%m-%d %H:%M:%S")
-        if t > date:
+        if cmpDate > date:
             continue 
        
         if line.count("CASH_LOAN") == 0:
